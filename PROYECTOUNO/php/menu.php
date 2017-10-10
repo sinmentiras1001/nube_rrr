391291c96bb9e48eb10dbb9ddc67f2097655d8d4
Estudiante <Estudiante@UIT-511>
2017-10-10 12:49:42 -0500

    Estas es mi parte con la carpeta PROYECTOUNO elaborada por Johan Delgado

---
+++ PROYECTOUNO/php/menu.php
@@ -0,0 +1,10 @@
+ <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo right"><i class="material-icons"></i>Johan</a>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="humano.php"><i class="icon-child"></i></a></li>
+        <li><a href="airport-plane.php"><i class="icon-airport-plane"></i></a></li>
+        <li><a href="battery-3.php"><i class="icon-battery-3"></i></a></li>        
+      </ul>
+    </div>
+  </nav>
---
+++ PROYECTOUNO/php/menu_desplegable.php
@@ -0,0 +1,30 @@
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">Primero Uno</a></li>
+  <li><a href="#!">Primero Dos</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Primero Tres</a></li>
+</ul>
+<ul id="dropdown2" class="dropdown-content">
+  <li><a href="#!">Segundo Uno</a></li>
+  <li><a href="#!">Segundo Dos</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Segundo Tres</a></li>
+</ul>
+<ul id="dropdown3" class="dropdown-content">
+  <li><a href="#!">Tercero Uno</a></li>
+  <li><a href="#!">Tercero Dos</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Tercero Tres</a></li>
+</ul>
+<nav>
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">JOHAN</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1"><i class="material-icons right" href="humano.php"><i class="icon-child">Primero</i></a></li>
+        <li><a class="dropdown-button" href="#!" data-activates="dropdown2"><i class="material-icons right" href="airport-plane.php"><i class="icon-airport-plane">Segundo</i></a></li>
+        
+      <!-- Dropdown Trigger -->
+      <li><a  class="dropdown-button" href="#!" data-activates="dropdown3"><i class="material-icons right" href="battery-3.php"><i class="icon-battery-3">Tercero</i></a></li>
+    </ul>
+  </div>
+</nav>
